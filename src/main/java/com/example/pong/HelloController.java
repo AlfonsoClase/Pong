240be78df2d17791754cd758b4130d6c7333c78b
AlfonsoClase <114950462+AlfonsoClase@users.noreply.github.com>
2023-02-10 12:35:35 +0100

    FaltaBola

--- src/main/java/com/example/pong/HelloController.java
+++ src/main/java/com/example/pong/HelloController.java
@@ -1,40 +1,38 @@
 package com.example.pong;
 
-import javafx.event.EventHandler;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 public class HelloController {
-    private Rectangle Racquet1, Racquet2;
+    @FXML
+    private Rectangle racquet1, racquet2;
+    @FXML
     private Circle ball;
 
-    private Pane panel;
+    @FXML
+    public void initialize() {
 
+        ball.setCenterX(ball.getCenterX() + 130);
+        ball.setCenterY(ball.getCenterY() + 130);
+        ball.sceneProperty().addListener((observableValue, scene, t1) -> t1.setOnKeyPressed(keyEvent ->
+        {
 
+            if (keyEvent.getCode() == KeyCode.UP) {
+                racquet2.setY(racquet2.getY() - 10);
+            } else if (keyEvent.getCode() == KeyCode.DOWN) {
+                racquet2.setY(racquet2.getY() + 10);
+            } else if (keyEvent.getCode() == KeyCode.W) {
+                racquet1.setY(racquet1.getY() - 10);
+            } else if (keyEvent.getCode() == KeyCode.S) {
+                racquet1.setY(racquet1.getY() + 10);
+            }
+        }));
 
-
-   public void initialize(){
-
-       ball.getScene().setOnKeyPressed(keyEvent ->
-       {
-           if (keyEvent.getCode() == KeyCode.UP){
-               Racquet2.setY(Racquet2.getY() - 10);
-           } else if (keyEvent.getCode() == KeyCode.DOWN)
-           {
-               Racquet2.setY(Racquet2.getY() + 10);
-           } else if (keyEvent.getCode() == KeyCode.W)
-           {
-               Racquet1.setY(Racquet1.getY() - 10);
-           } else if (keyEvent.getCode() == KeyCode.S)
-           {
-               Racquet1.setY(Racquet1.getY() + 10);
-           }
-       });
-
-   }
+    }
 }
--- src/main/java/com/example/pong/Pong.java
+++ src/main/java/com/example/pong/Pong.java
@@ -1,28 +1,45 @@
 package com.example.pong;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 public class Pong extends Application {
 
+    private  Circle pelota;
+
+    private int velX = 1;
+    private int velY = 1;
+
     @Override
-    public void start(Stage primaryStage){
+    public void start(Stage stage) throws IOException {
 
-        FXMLLoader loader = new FXMLLoader();
-
-        primaryStage.setTitle("Pong Juego");
-        primaryStage.show();
+        FXMLLoader fxmlLoader = new FXMLLoader(Pong.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Pong");
+        stage.setScene(scene);
+        stage.show();
 
     }
 
     public static void main(String[] args) {
-        launch(args);
+
+        launch();
+
     }
 }
